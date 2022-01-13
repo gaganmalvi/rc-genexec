0515428b58b6ea4eab5cd37eef21e96d5078165b
docflex <rehbermoin.io@gmail.com>
2022-01-14 01:34:38 +0530

    Added Xor-Duplicates

--- xor-duplicates.cpp
+++ xor-duplicates.cpp
@@ -19,8 +19,6 @@ void solveMyProblem(long long n, vector<long long> &arr) {
 }
 
 int main() {
-    // freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
     FastIO;
     try {
         long long n;
