9db416ef921ec586ce3e79809baaaeac274acebd
chasmiccoder <chasmiccoder@gmail.com>
2022-01-15 03:53:38 +0530

    Finished Qs

--- combinations.cpp
+++ combinations.cpp
@@ -1,27 +1,39 @@
 #include<iostream>
-using namespace std;
+
+#define FastIO std::ios_base::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL)
 
 long long factorial(int x) {
     long long result = 1;
     for(int i = 1; i <= x; i++) {
-        result *= i;
+        result *= (long long) i;
     }
     return result;
 }
 
 int main() {
+    FastIO;
     
     try {
-        int n,r;
-        cin >> n >> r;
-        if(!cin || n < 0 || n > 20 || r < 0 || r > 20 || r > n) {
+        int test;
+        std::cin >> test;
+        if(!std::cin || test < 1 || test > 100) {
             throw -1;
         }
 
-        long long ans = factorial(n) / (factorial(n-r) * factorial(r));
-        cout << ans << "\n";
+        for(int t = 0; t < test; t++) {
+            int a,b;
+            std::cin >> a >> b;
+            
+            if(!std::cin || a < 0 || a > 20 || b < 0 || b > 20 || b > a) {
+                throw -2;
+            }
+
+            long long ans = factorial(a) / (factorial(a-b)*factorial(b));
+            std::cout << ans << "\n";            
+
+        }
     } catch(...) {
-        cout << "Invalid input. Please refer to the question's description.\n";
+        std::cout << "Invalid input. Please refer to the question's description.\n";
     }
 
     return 0;
