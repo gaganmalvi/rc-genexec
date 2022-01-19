084e284fcf4779707593359411f04612a219d042
chasmiccoder <chasmiccoder@gmail.com>
2022-01-19 11:11:49 +0530

    Fixed permutations Q

--- permutations.cpp
+++ permutations.cpp
@@ -8,33 +8,50 @@ using namespace std;
 #define el   '\n'
 // clang-format on
 
-long fact(long n) {
-    long ans = 1;
-    for (long i = 1; i <= n; i++)
+long long fact(int n) {
+    long long ans = 1;
+    for (long long i = 1; i <= n; i++)
         ans *= i;
     return ans;
 }
 
-int perm(string str) {
+long long perm(string str) {
     size_t length = str.length();
     vector <int> freq(26, 0);
-    for (int i = 0; i < length; i++)
-        if (str[i] >= 'a')
-            freq[str[i] - 'a']++;
-    int f = 1;
+    for (int i = 0; i < length; i++) {
+        freq[str[i] - 'a']++;
+    }
+    long long f = 1;
     for (int i = 0; i < 26; i++)
         f *= fact(freq[i]);
-    return (int)fact((long)length) / f;
+    return fact((long long)length) / f;
 }
 
 int32_t main() {
     FastIO;
     try {
-        string s;
-        cin >> s;
-        if (!cin || s.length() < 1 || s.length() > 100000)
-            throw -1;
-        cout << perm(s) << el;
+    	int test;
+    	cin >> test;
+    	if(!cin || test < 1 || test > 50000) {
+	    throw -1;
+	}
+    	
+    	for(int t = 0; t < test; t++) {
+            string s;
+            cin >> s;
+            int n = s.length();
+            if (!cin || n < 1 || n > 20)
+                throw -1;
+            
+            // check if all letters are lowercase
+            for(int i = 0; i < n; i++) {
+                if(s[i] < 'a' || s[i] > 'z') {
+                    throw -2;
+                }
+            }
+            long long res = perm(s);
+            cout << res << "\n";
+        }
     } catch (...) {
         cout << "Invalid input. Please refer to the question description." << endl;
         return 0;
