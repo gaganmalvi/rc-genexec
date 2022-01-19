de4972f92d1b23ebc2596d5ccb43e039f5244bfd
docflex <77137467+docflex@users.noreply.github.com>
2022-01-20 00:22:14 +0530

    Update: Improved Overhead. Refactored perm().

--- permutations.cpp
+++ permutations.cpp
@@ -1,7 +1,6 @@
 // clang-format off
 #include <iostream>
 #include <string>
-#include <math.h>
 #include <vector>
 using namespace std;
 #define FastIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
@@ -16,11 +15,10 @@ long long fact(int n) {
 }
 
 long long perm(string str) {
-    size_t length = str.length();
-    vector <int> freq(26, 0);
-    for (int i = 0; i < length; i++) {
+    long long length = (long long)str.length();
+    vector<int> freq(26, 0);
+    for (int i = 0; i < length; i++)
         freq[str[i] - 'a']++;
-    }
     long long f = 1;
     for (int i = 0; i < 26; i++)
         f *= fact(freq[i]);
@@ -30,30 +28,23 @@ long long perm(string str) {
 int32_t main() {
     FastIO;
     try {
-    	int test;
-    	cin >> test;
-    	if(!cin || test < 1 || test > 50000) {
-	    throw -1;
-	}
-    	
-    	for(int t = 0; t < test; t++) {
+        int test;
+        cin >> test;
+        if (!cin || test < 1 || test > 50000)
+            throw -1;
+        for (int t = 0; t < test; t++) {
             string s;
             cin >> s;
             int n = (int)s.length();
             if (!cin || n < 1 || n > 20)
                 throw -1;
-            
             // check if all letters are lowercase
-            for(int i = 0; i < n; i++) {
-                if(s[i] < 'a' || s[i] > 'z') {
+            for (int i = 0; i < n; i++)
+                if (s[i] < 'a' || s[i] > 'z')
                     throw -2;
-                }
-            }
             long long res = perm(s);
-            cout << res << "\n";
+            cout << res << el;
         }
-    } catch (...) {
-        cout << "Invalid input. Please refer to the question description." << endl;
-        return 0;
-    }
+    } catch (...) { cout << "Invalid input. Please refer to the question description." << el; }
+    return 0;
 }
